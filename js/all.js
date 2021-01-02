29f43d5b12c14de38a399fe87e3de81da04d3f44
hereiswenning <hereiswenning@gmail.com>
2021-01-02 22:06:58 +0800

    User testing

--- js/all.js
+++ js/all.js
@@ -6,3 +6,37 @@ function menuToggle(){
         x.className = 'navtoggle';
     }
 }
+
+window.onscroll = function(){slideUp();}
+
+function slideUp(){
+    if(document.body.scrollTop > 900 || document.documentElement.scrollTop > 900){
+    document.getElementById("project_side_bar").className = "slideUp";
+        if(document.body.scrollTop > 900 && document.body.scrollTop < 1500){
+            document.getElementById("color1").className = "accent";
+        }else{
+            document.getElementById("color1").className = "caption";
+        }
+    }else{
+    document.getElementById("project_side_bar").className = "";
+    }
+}
+
+// window.onscroll = function(){accent_animation();}
+
+// function accent_animation(){
+//     if(document.body.scrollTop > 0 || document.documentElement.scrollTop > 0){
+//     document.getElementById("accent-animation").className = "accent_animation2";
+        
+//     }else{
+//     document.getElementById("accent-animation").className = "accent_animation";
+//     }
+// }
+
+// window.onscroll = function(){emergeUp};
+
+// function emergeUp(){
+//     if(document.animate.)
+// }
+
+
